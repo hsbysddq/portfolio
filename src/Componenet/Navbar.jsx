43663c7d5f30794f779a796844976439485c0be9
hsbysddq <hsby15sddq@gmail.com>
2023-08-12 23:58:47 +0700

    edit font

--- src/Componenet/Navbar.jsx
+++ src/Componenet/Navbar.jsx
@@ -2,13 +2,13 @@ function Navbar() {
   return (
     <nav className="bg-black  flex justify-between text-[#999999] pt-3 pb-3 px-14">
       <div className="logo p-4">
-        <a href="/" className="site-tittle flex">
+        <a href="/" className="site-tittle flex font-poppins">
           Hasbi's
         </a>
       </div>
 
       <div className="list">
-        <ul className="flex list-none gap-4">
+        <ul className="flex list-none gap-4 font-poppins">
           <li className="p-4">
             <a href="/">About</a>
           </li>
@@ -22,7 +22,7 @@ function Navbar() {
             <a href="/">Contact</a>
           </li>
           <li className="p-3">
-            <button className="text-xs text-[#FFB800] outline outline-offset-2 outline-1 rounded-sm px-4 py-2">
+            <button className="text-xs text-[#ffb800] outline outline-offset-2 outline-1 rounded-sm px-4 py-2">
               Resume
             </button>
           </li>
--- src/Componenet/Tagline.jsx
+++ src/Componenet/Tagline.jsx
@@ -1,22 +1,23 @@
 function Tagline() {
   return (
-    <div className="pt-24 flex flex-row">
+    <div className="pt-24 flex flex-row font-poppins">
       <div className="basis-1/6"></div>
 
       <div className="basis-2/3">
-        <p className="text-6xl text-[#FFB800] pb-8 font-bold  text-center">
+        <p className="text-6xl text-[#ffb800] pb-8 font-extrabold  text-center">
           HELLO <br /> I BUILD THINGS <br /> FOR THE WEBSITE
         </p>
 
         <p className="text-l text-[#BDBDBD] pb-14  text-center">
           I’m a software engineer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building accessible, human-centered products at Upstatement.
+          designing) <br /> exceptional digital experiences. Currently, I’m
+          focused on building <br /> accessible, human-centered products at
+          Upstatement.
         </p>
 
         <div className="text-center">
           <a href="/" className="text-center">
-            <button className="text-[#FFB800] outline outline-offset-2 outline-1 rounded-sm px-6 py-3 text-center ">
+            <button className="text-[#ffb800] outline outline-offset-2 outline-1 rounded-sm px-6 py-3 text-center ">
               Check My Portfolio!
             </button>
           </a>
