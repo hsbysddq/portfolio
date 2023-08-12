b7e81a4e7f5005ec11fe66336fd71f839fd2ce53
hsbysddq <hsby15sddq@gmail.com>
2023-08-12 23:58:29 +0700

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       backgroundColor: {
         navy: "#8892B0f",
       },
