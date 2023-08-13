5c1fce5888a3921329733a85dacc99630c4b7545
hsbysddq <hsby15sddq@gmail.com>
2023-08-13 23:14:44 +0700

    add project feature

---
+++ src/Pages/Project.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Project = () => {
+  return (
+    <div className="pl-44 pt-36 w-10/12">
+      <div className=" text-[#999999] font-poppins">
+        <h1 className="text-4xl font-bold uppercase text-center pb-12">
+          Some Things I’ve Built
+        </h1>
+
+        <div className="mb-96 flex justify-center">
+          <div className="bg-[#ffb800] h-20 w-6/12 rounded-3xl"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Project
