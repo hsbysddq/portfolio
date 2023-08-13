975c5beed48c3f3798f45774bdbb5d1d8ec415b2
hsbysddq <hsby15sddq@gmail.com>
2023-08-13 22:22:33 +0700

    add icon navbar

--- src/Componenet/Navbar.jsx
+++ src/Componenet/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { FiArrowUpRight } from "react-icons/fi"
 function Navbar() {
   return (
     <nav className="bg-black  flex justify-between text-[#999999] pt-3 pb-3 px-14">
@@ -11,19 +12,23 @@ function Navbar() {
 
       <div className="list">
         <ul className="flex list-none gap-4 font-poppins">
-          <li className="p-4 hover:text-[#ffb800]">
+          <li className="flex p-4 hover:text-[#ffb800]">
             <a href="#about">About</a>
+            <FiArrowUpRight className="w5 h-5 ml-2 mt-0.5" />
           </li>
-          <li className="p-4 hover:text-[#ffb800]">
+          <li className="flex p-4 hover:text-[#ffb800]">
             <a href="/">Experience</a>
+            <FiArrowUpRight className="w5 h-5 ml-2 mt-0.5" />
           </li>
-          <li className="p-4 hover:text-[#ffb800]">
+          <li className="flex p-4 hover:text-[#ffb800]">
             <a href="/">Work</a>
+            <FiArrowUpRight className="w5 h-5 ml-2 mt-0.5" />
           </li>
-          <li className="p-4 hover:text-[#ffb800]">
+          <li className="flex p-4 hover:text-[#ffb800]">
             <a href="/">Contact</a>
+            <FiArrowUpRight className="w5 h-5 ml-2 mt-0.5" />
           </li>
-          <li className="p-3">
+          <li className="flex p-3">
             <button className="text-xs text-[#ffb800] outline outline-offset-2 outline-1 rounded-sm px-4 py-2">
               Resume
             </button>
