0db2ed7bd6511523feb90e15cb24847dcde26c5d
hsbysddq <hsby15sddq@gmail.com>
2023-08-13 23:14:27 +0700

    add ject routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Homepage from "./Pages/Homepage"
 import About from "./Pages/About"
+import Project from "./Pages/Project"
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
       </Routes>
     </Router>
   )
