416e8fe3df2f147c06071155cbd6b2b6bb827118
hsbysddq <hsby15sddq@gmail.com>
2023-08-14 14:01:43 +0700

    set up navigasi nav

--- src/Componenet/Navbar.jsx
+++ src/Componenet/Navbar.jsx
@@ -1,39 +1,39 @@
-import { Link } from "react-router-dom"
 import { FiArrowUpRight } from "react-icons/fi"
 function Navbar() {
   return (
-    <nav className="bg-black  flex justify-between text-[#999999] pt-3 pb-3 px-14">
-      <div className="logo p-4 hover:text-[#ffb800]">
-        <Link to="/">Hasbi's</Link>
-        {/* <a href="/" className="site-tittle flex font-poppins">
-          Hasbi's
-        </a> */}
+    <nav className="bg-transparent  flex  justify-between text-[#999999] pt-4 pb-3 px-14">
+      <div className="logo p-4 font-poppins hover:text-[#ffb800] font-semibold text-md">
+        <a
+          href="/"
+          className="site-tittle flex align-center
+        "
+        >
+          H A S B Y
+        </a>
       </div>
 
       <div className="list">
-        <ul className="flex list-none gap-4 font-poppins">
+        <ul className="flex list-none gap-4 font-poppins justify-center">
           <li className="flex p-4 hover:text-[#ffb800]">
             <a href="#about">About</a>
-            <FiArrowUpRight className="w5 h-5 ml-2 mt-0.5" />
           </li>
           <li className="flex p-4 hover:text-[#ffb800]">
             <a href="/">Experience</a>
-            <FiArrowUpRight className="w5 h-5 ml-2 mt-0.5" />
           </li>
           <li className="flex p-4 hover:text-[#ffb800]">
-            <a href="/">Work</a>
-            <FiArrowUpRight className="w5 h-5 ml-2 mt-0.5" />
+            <a href="#project">Work</a>
           </li>
           <li className="flex p-4 hover:text-[#ffb800]">
-            <a href="/">Contact</a>
+            <a href="mailto:hsby15sddq@gmail.com">Contact</a>
             <FiArrowUpRight className="w5 h-5 ml-2 mt-0.5" />
-          </li>
-          <li className="flex p-3">
-            <button className="text-xs text-[#ffb800] outline outline-offset-2 outline-1 rounded-sm px-4 py-2">
-              Resume
-            </button>
           </li>
         </ul>
+      </div>
+
+      <div className="portfolio font-poppins justify-center p-3">
+        <button className="text-xs text-[#ffb800] outline outline-offset-2 outline-1 rounded-sm px-4 py-2">
+          Resume
+        </button>
       </div>
     </nav>
   )
